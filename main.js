557d3112b14968a44c30e6021263ecb4cc4b1462
cjshallow <cjshallow@gmail.com>
2024-07-14 17:23:45 -0400

    Added Functionality to addBookToLibrary Function

--- main.js
+++ main.js
@@ -10,11 +10,9 @@ function Book(title, author, pages, readStatus) {
 // loop that takes each item of array and displays it in an element, in a function.
 // clicking add book button triggers addition
 
-function render() {
-    // loop to look through array
-    // pull items from array and display them in card in new element
-    // can put html within this function
-}
+function displayBooks() {
+
+} 
 
 
 function addBookToLibrary() {
@@ -24,7 +22,7 @@ function addBookToLibrary() {
     let readStatus = document.querySelector("#read").value;
     let newBook = new Book(title, author, pages, readStatus)
     myLibrary.push(newBook);
-    render();
+    displayBooks();
 }
 
 
